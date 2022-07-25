1587654c6ec0424c847d87a9c310655c44cf924f
Jacob Prebys <jprebys@sgcaptrading.com>
2022-07-25 12:44:56 -0500

    Initial commit, sketch out main function

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 128
+
+char *get_stdin_line()
+{
+	char *buffer = NULL;
+	size_t n = 0;
+
+	if (getline(&buffer, &n, stdin) == -1) {
+		if (feof(stdin))
+			return buffer;
+		else {
+			perror("Error using getline");
+			exit(1);
+		}
+	}
+	return buffer;
+
+}
+
+
+void jsh_main_loop_run()
+{
+	char *line;
+	do {
+		line = get_stdin_line();
+		printf("%s", line);
+	} while (strcmp(line, "exit\n"));
+	printf("Exiting...\n");
+}
+
+int main()
+{
+	// TODO: parse command line args
+	// TODO: read config file here
+
+	// run main shell loop
+	jsh_main_loop_run();
+
+	// TODO: add any cleanup here
+
+	return 0;
+}
